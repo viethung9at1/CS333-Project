eaeb9913a55edaee6b52614d51aaacf753f01682
baoloi2002 <loitan1042002@gmail.com>
2023-03-17 09:01:25 +0700

    fix

--- NachOS-4.0/code/test/createfile.c
+++ NachOS-4.0/code/test/createfile.c
@@ -1,19 +1,16 @@
 #include "syscall.h" 
 #include "copyright.h" 
 #define maxlen 32 
-int 
-main() 
-{ 
-    int len; 
-    char filename[maxlen +1]; 
-    /*Create a file*/ 
-    if (Create("text.txt") == -1) 
-    { 
-    // xuất thông báo lỗi tạo tập tin 
-    } 
-    else 
-    { 
-    // xuất thông báo tạo tập tin thành công 
-    } 
+int  main(int argc, char* argv[]) { 
+    
+    if (argc == 1){
+
+    }else
+    if (argc == 2){
+        Create(argv[1]);
+    }else{
+        printf("The format is wrong !");
+    }
+
     Halt(); 
 } 
