12665ca22b0af09454ab9a4cb3840e57676201bb
viethung9at1 <viethung.9at1@gmail.com>
2023-05-07 01:28:32 +0700

    Co-authored-by: Tran Bao Loi <baoloi2002@users.noreply.github.com>

---
+++ NachOS-4.0/code/test/shellTest.c
@@ -0,0 +1,14 @@
+#include "syscall.h"
+#include "copyright.h" 
+
+int main()
+{
+    SpaceId newProc1;
+    SpaceId newProc2;
+
+    newProc1 = Exec("cat"); // Project 01
+    newProc2 = Exec("copy"); // Project 01
+
+    Join(newProc1);
+    Join(newProc2);
+}
